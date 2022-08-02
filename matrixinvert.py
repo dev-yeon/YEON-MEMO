1aee013e7eb2dae84b08287671a430424bcd9836
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2022-08-03 03:21:38 +0900

    Create matrixinvert.py

---
+++ matrixinvert.py
@@ -0,0 +1,27 @@
+## template
+
+def invertmatrix(matrix, i, j):
+  if matrix[i][j] == 1:
+    matrix[i][j] = 0
+  elif matrix[i][j] == 0:
+    matrix[i][j] = 1
+  return matrix
+
+
+n = int(input())
+matrix = [[0 for _ in range(10)] for _ in range(10)]
+
+for i in range(n):
+  for j in range(10):
+    matrix = invertmatrix(matrix, i, j)
+  for k in range(10):
+    matrix = invertmatrix(matrix, k, i)
+  matrix = invertmatrix(matrix, i, i)
+
+  
+for i in range(10):
+  tempstr = ""
+  for j in range(10):
+    tempstr += str(matrix[i][j])
+    tempstr += " "
+  print(tempstr)
